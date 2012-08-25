49bc5f4f2d6a377b1b14e553065584f609862cd5
Jay Hannah <jay.hannah@iinteractive.com>
2012-08-25 15:42:34 -0500

    Committing from CPAN 0.12 tarball.

--- lib/Net/Bullfinch/Iterator.pm
+++ lib/Net/Bullfinch/Iterator.pm
@@ -1,4 +1,7 @@
 package Net::Bullfinch::Iterator;
+{
+  $Net::Bullfinch::Iterator::VERSION = '0.12';
+}
 use Moose;
 
 # ABSTRACT: A way to iterator over results from Bullfinch
@@ -9,32 +12,6 @@ use Net::Bullfinch;
 
 with 'Data::Stream::Bulk::DoneFlag';
 
-=head1 DESCRIPTION
-
-This module provides a way to iterate over a result stream
-coming from Net::Bullfinch. It uses the Data::Stream::Bulk
-role and so therefore has all the functionality implied therein.
-
-=head1 SYNOPSIS
-
-    use Net::Bullfinch;
-
-    my $client = Net::Bullfinch->new(host => '172.16.49.130');
-    my $req = { statement => 'some-query' };
-    my $items = $client->iterate(
-        request_queue => 'test-net-kestrel',
-        request => $req,
-        response_queue_suffix => 'foobar',
-        iterator_options => { max_results => 200 }
-    );
-
-    while ( my $block = $items->next ) {
-        foreach my $item ( @$block ) {
-            # do something with each item ...
-        }
-    }
-
-=cut
 
 has 'bullfinch' => (
     is       => 'ro',
@@ -126,4 +103,54 @@ __PACKAGE__->meta->make_immutable;
 1;
 
 
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Net::Bullfinch::Iterator - A way to iterator over results from Bullfinch
+
+=head1 VERSION
+
+version 0.12
+
+=head1 SYNOPSIS
+
+    use Net::Bullfinch;
+
+    my $client = Net::Bullfinch->new(host => '172.16.49.130');
+    my $req = { statement => 'some-query' };
+    my $items = $client->iterate(
+        request_queue => 'test-net-kestrel',
+        request => $req,
+        response_queue_suffix => 'foobar',
+        iterator_options => { max_results => 200 }
+    );
+
+    while ( my $block = $items->next ) {
+        foreach my $item ( @$block ) {
+            # do something with each item ...
+        }
+    }
+
+=head1 DESCRIPTION
+
+This module provides a way to iterate over a result stream
+coming from Net::Bullfinch. It uses the Data::Stream::Bulk
+role and so therefore has all the functionality implied therein.
+
+=head1 AUTHOR
+
+Cory G Watson <gphat@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Infinity Interactive, Inc.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
